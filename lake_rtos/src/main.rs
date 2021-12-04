917c9ec15d82882bd999961d96ae59db91ccad05
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2021-12-04 10:01:18 +0100

    wip: doc

--- lake_rtos/src/main.rs
+++ lake_rtos/src/main.rs
@@ -1,11 +1,17 @@
+//!
+//! Main crate for Kernelimplementation. Depends on lake_rtos_rt
+//! and the main function gets called after successful power on. 
+//!
 #![no_std]
 #![no_main]
 
 extern crate lake_rtos_rt;
 
+///
+/// Target function after hardware initialization, 
+/// acts as the first kernel function.
+/// 
 #[no_mangle]
-pub fn main() -> ! {
-    let _x = 42;
-    // DOC COMMING SOON
+pub fn kmain() -> ! {
     loop {}
 }
