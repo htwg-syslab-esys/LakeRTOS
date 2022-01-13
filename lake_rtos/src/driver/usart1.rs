1c0a615258cb430ae6d928a8c439afb946854655
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2022-01-13 09:43:15 +0100

    wip: comments

--- lake_rtos/src/driver/usart1.rs
+++ lake_rtos/src/driver/usart1.rs
@@ -1,3 +1,11 @@
+//! # USART1
+//!
+//! This module offers a basic implementation of the user interface. 
+//! When creating the object, the device is initialized according to the baud rate.
+//! GPIO pin 9 is also will be configured according to the necessary specifications.
+//! This class offers as a special feature an implementation of iostream, so that both string and integer values ​​
+//! can be transmitted directly.
+//! 
 use crate::dp::gpio::GPIO;
 use crate::dp::uart::UART;
 
@@ -66,9 +74,19 @@ pub struct USART1 {
 }
 
 impl USART1 {
+    /// Returns a new USART Device
+    ///
+    /// # Arguments
+    ///
+    /// * `gpioa` - A static mutable reference to a gpio registerblock
+    /// * `baudrate` - An u32 for desired baud rate
+    ///
+    /// # Returns
+    /// * `USART1` - The USART1 Struct
+    ///
     pub fn new(
         gpioa: &'static mut GPIO,
-        /* uart: &'static mut UART, */ baudrate: u32,
+        baudrate: u32,
     ) -> USART1 {
         USART1 {
             gpio: gpioa,
@@ -78,13 +96,16 @@ impl USART1 {
     }
 
     pub fn init(&mut self) -> &mut USART1 {
-        //af
+        // This operation turns pin 9 into alternate function mode
         self.gpio.moder.set_bit(19);
-        // pushpull
+        // When using GPIO as transmit pin it has to be configured as Push-Pull output
         self.gpio.otyper.clear_bit(9);
-        // af7
+        // The associated alternate function for pin- and usart-tx combination is af7
         self.gpio.afrh.replace_bits(4, 7, 4);
 
+        // Basic configuration of usart. We calculate the baud rate according to the user's manual seection 29.5.4,
+        // activate transmitter and receiver and finally activate the usart controller.
+        // Note: these settings only can be set when the controller is disabled
         self.uart
             .brr
             .replace_bits(0, (8_000_000 / self.baudrate), 32);
