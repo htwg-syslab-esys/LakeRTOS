8d4d0514b47eb308148a53418d186e06a3f69c4b
turbokalle <lukas.kaluscha@gmail.com>
2022-01-17 16:41:36 +0100

    updated __context_switch parametername

--- lake_rtos/src/kernel/exceptions.rs
+++ lake_rtos/src/kernel/exceptions.rs
@@ -28,6 +28,6 @@ pub fn trigger_PendSV() {
 /// there are nested exceptions.
 #[no_mangle]
 pub unsafe extern "C" fn PendSV() {
-    let (psp_next_addr, psp_current_addr) = CONTEXT_SWITCH.get_addr_and_swap();
-    __context_switch(psp_next_addr, psp_current_addr);
+    let (psp_next_addr, psp_from_addr) = CONTEXT_SWITCH.get_addr_and_swap();
+    __context_switch(psp_next_addr, psp_from_addr);
 }
--- lake_rtos/src/kernel/mod.rs
+++ lake_rtos/src/kernel/mod.rs
@@ -8,7 +8,7 @@ mod cs;
 
 extern "C" {
     /// The context switch only works when called within an interrupt. e.g. [exceptions::PendSV]
-    pub fn __context_switch(psp_next_addr: u32, psp_current_addr: u32);
+    pub fn __context_switch(psp_next_addr: u32, psp_from_addr: u32);
     /// Sets a breakpoint in the running program.
     pub fn __breakpoint();
 }
